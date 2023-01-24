c84d603785973b982b5a8ed271ce530c1bcf1fc1
Reagan Kelly <ylb9@cdc.gov>
2023-01-24 13:19:26 -0500

    Update .dockerignore and singularity build script

--- build_singularity.sh
+++ build_singularity.sh
@@ -4,15 +4,15 @@
 # Description:
 # Usage:
 # Date Created: 2023-01-11 13:28
-# Last Modified: Wed 11 Jan 2023 01:34:16 PM EST
+# Last Modified: Tue 24 Jan 2023 10:00:57 AM EST
 # Author: Reagan Kelly (ylb9@cdc.gov)
 #
 
 if [[ ${1} == 'with_references' ]]; then
     echo 'Building singularity image with references included'
-    singularity build pipeline_with_references.sif docker://ghcr.io/cdcgov/varpipe_wgs_with_refs:latest
+    SINGULARITY_TMPDIR=~/.tmp singularity build pipeline_with_references.sif docker://ghcr.io/cdcgov/varpipe_wgs_with_refs:latest
 else
     echo 'Building singularity image without references included'
-    singularity build pipeline_with_references.sif docker://ghcr.io/cdcgov/varpipe_wgs_without_refs:latest
+    SINGULARITY_TMPDIR=~/.tmp singularity build pipeline_with_references.sif docker://ghcr.io/cdcgov/varpipe_wgs_without_refs:latest
 fi
 
