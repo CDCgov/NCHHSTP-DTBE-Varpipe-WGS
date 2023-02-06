1d512ce4e8ea4381b843b6d505ca7478987695b9
Reagan Kelly <ylb9@cdc.gov>
2023-02-06 17:44:51 -0500

    Fix typo in get_clockwork script

--- get_clockwork.sh
+++ get_clockwork.sh
@@ -4,8 +4,8 @@
 # Description:
 # Usage:
 # Date Created: 2023-01-17 14:43
-# Last Modified: Mon 23 Jan 2023 08:22:04 PM EST
+# Last Modified: Mon 06 Feb 2023 05:43:51 PM EST
 # Author: Reagan Kelly (ylb9@cdc.gov)
 #
 
-wget -O- https://github.com/iqbal-lab-org/clockwork/releases/download/v0.11.3/clockwork_v0.11.3.img >tools/clockwork-0.11.3/clockwork_v01.11.3.img
+wget -O- https://github.com/iqbal-lab-org/clockwork/releases/download/v0.11.3/clockwork_v0.11.3.img >tools/clockwork-0.11.3/clockwork_v0.11.3.img
